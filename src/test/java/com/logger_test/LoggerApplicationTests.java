98740f31dc7e05b2bfeed7e6f6a45679a9f9d645
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-01-25 10:17:38 +0530

    new

---
+++ src/test/java/com/logger_test/LoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logger_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
