e1602e33a41c17490b3a7937103187e0e1937d83
Solomonraja-datasirpi <155154249+Solomonraja-datasirpi@users.noreply.github.com>
2024-02-02 16:41:32 +0530

    Update NamingconventionApplication.java

--- naming_convention_springboot/src/main/java/com/example/namingconvention/NamingconventionApplication.java
+++ naming_convention_springboot/src/main/java/com/example/namingconvention/NamingconventionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class NamingconventionApplication {
+public class NamingConventionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NamingconventionApplication.class, args);
